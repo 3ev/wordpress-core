8e8ba3ea920525b8fc83b82cc088864aecfc57cb
Ben Constable <ben@benconstable.co.uk>
2015-02-12 11:45:53 +0000

    Add a slug utility method

--- src/Tev/Database/Utils.php
+++ src/Tev/Database/Utils.php
@@ -31,4 +31,27 @@ class Utils
 
         return $placeholders;
     }
+
+    /**
+     * Generate a slug from the given field.
+     *
+     * Uses `sanitize_title()` to create a slug. If $unique is provided, this
+     * method will check if the slug already exists in $unique. If it does,
+     * it will append '-1', '-2' etc to the slug until a unique value is found.
+     *
+     * @param  string $data   Data to slugify
+     * @param  array  $unique Array of values to ensure slug is unique against
+     * @return string         Generated slug
+     */
+    public function createSlug($data, array $unique = array())
+    {
+        $slug = sanitize_title($data);
+        $next = 1;
+
+        while (in_array($slug, $unique)) {
+            $slug = sanitize_title($data . ' ' . $next++);
+        }
+
+        return $slug;
+    }
 }
